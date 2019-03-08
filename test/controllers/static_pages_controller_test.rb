0117a50804781c6692d0e4443a38ea40794803cf
Your Name <your.email@example.com>
2019-03-08 21:50:31 +0700

    first commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,9 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-	def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+	#def setup
+    #@base_title = "Ruby on Rails Tutorial Sample App"
+  #end
+
+    test "should get root" do
+    get FILL_IN
+    assert_response FILL_IN
   end
+
 
 
   test "should get home" do
@@ -24,6 +30,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
 
 end
 
